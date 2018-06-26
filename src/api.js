cc4c5bb9c1117b5e4d6a8c07c3d4b7d872a24633
Chandra Galbiati <chandra.galbiati@gmail.com>
2018-06-25 21:46:09 -0400

    Add outside api

--- src/api.js
+++ src/api.js
@@ -8,4 +8,11 @@ const fetchNameFirebase = () => (
   firebase.database().ref('test').once('value').then(snap => snap.val())
 );
 
-export default fetchNameFirebase;
+const fetchNameMicroservice = () => (
+  // fetch('https://api.onepeloton.com/api/ride/27b13f0cf4a64eac964d6c54347b30bb/details')
+  fetch("/api/ride/27b13f0cf4a64eac964d6c54347b30bb/details")
+    .then(res => res.json())
+    .then(json => json.ride.title)
+);
+
+export default fetchNameMicroservice;
